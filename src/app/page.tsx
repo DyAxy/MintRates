f873c1850b229ba7edc8bc0445b069b394268341
DyAxy <haruka@dy.ax>
2025-06-01 21:40:32 +0800

    feat: 为货币列表组件添加唯一键，以优化渲染性能

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-start h-dvh bg-[var(--tg-theme-bg-color)]">
       <div className="flex flex-col w-full">
-        {data && <CurrencyList rateData={data} />}
+        {data && <CurrencyList key={rateId} rateData={data} />}
       </div>
       <footer className="w-full">
         <RateFooter rateId={rateId} setRateId={setRateId} />
